91b415eebf70c07536c95bb6da0e8db8c5063033
mikeycab3 <miguelcabreramunoz287@gmail.com>
2026-04-21 15:23:25 -0600

    se agrega librerias para iconos

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItems } from '../../models/cartItem';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCartShopping, faHeart, faMagnifyingGlass, faUser, faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  faCart = faCartShopping;
+  faHeart = faHeart;
+  faSearch = faMagnifyingGlass;
+  faUser = faUser;
+  faMenu = faBars;
   @Input() items: CartItems [] = [];
   @Output() openEventEmitter = new EventEmitter();
 
